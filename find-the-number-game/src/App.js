e9a9eff1c78109f0135c434ec67cfc78bd2e2e72
Eduard Gaidukevich <elladiy2000@yandex.ru>
2023-08-03 18:59:39 +0300

    version_1.0
    version_1.0

---
+++ find-the-number-game/src/App.js
@@ -0,0 +1,12 @@
+import './css/App.css';
+import MainSection from './js/MainSection';
+
+function App() {
+  return (
+    <div className="App">
+      <MainSection/>
+    </div>
+  );
+}
+
+export default App;
---
+++ find-the-number-game/src/js/MainSection.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const MainSection = () => {
+  return (
+    <div className="GameScreenMain">
+      <div className="GameScreenHeader">
+        <h3 className="GameScreenHeaderTitle">Найди число</h3>
+        <p className="GameScreenHeaderDescript">
+          <span className="ng-scope">Тренажер на внимание</span>
+        </p>
+      </div>
+      <div className="GameScreenBody">
+        <div className="GameScreenBodyContent">
+          <div className="GameScreenBodyWrap">
+            <h4 className="GameScreenBodyTitle">Тренирует:</h4>
+            <ul className="GameScreenList">
+              <li className="GameScreenListItem">
+                <div className="GameScreenListItemTitle">
+                  <div className="GameScreenListItemTitleImg eye1"></div>
+                  <div className="GameScreenListItemTitleText">
+                    Произвольное внимание
+                  </div>
+                </div>
+                <div className="GameScreenListItemDesc">
+                  Научитесь концентрировать внимание только на важном
+                </div>
+              </li>
+              <li className="GameScreenListItem">
+                <div className="GameScreenListItemTitle">
+                  <div className="GameScreenListItemTitleImg eye2"></div>
+                  <div className="GameScreenListItemTitleText">
+                    Концентрацию и переключение внимания
+                  </div>
+                </div>
+                <div className="GameScreenListItemDesc">
+                  Позволит не упускать из виду важные детали
+                </div>
+              </li>
+              <li className="GameScreenListItem">
+                <div className="GameScreenListItemTitle">
+                  <div className="GameScreenListItemTitleImg eye3"></div>
+                  <div className="GameScreenListItemTitleText">
+                    Зрительное восприятие
+                  </div>
+                </div>
+                <div className="GameScreenListItemDesc">
+                  Сможете быстро находить основные мысли в текстах
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="GameScreenDifficulty">
+            <a href="#lol" className="GameScreenBtn">Далее</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MainSection;
